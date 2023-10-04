f7dd2e52fbf7d1e86106186c02c53459cd9a8b91
Alesia <alesia.litvinenko@gmail.com>
2023-10-04 15:04:50 +0300

    refactor: edit header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import Routing from "pages";
-import "./index.css";
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import Routing from 'pages';
+import './index.css';
+import Header from 'widget/Header';
 
-const appTitle = "Fitness";
+const appTitle = 'Fitness';
 
 const App: React.FC = () => {
   return (
     <HelmetProvider>
       <Helmet defaultTitle={appTitle} titleTemplate={`%s | ${appTitle}`} />
+      <Header />
       <Routing />
     </HelmetProvider>
   );
